a6f5c19a958bb0bfd7e841dd4623efc3754bd2df
sumakallem <128606106+sumakallem@users.noreply.github.com>
2025-03-30 17:56:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["ap-south-1b", "ap-south-1a"] 
+    #availability_zones    = ["ap-south-1b", "ap-south-1a"] 
     launch_template {
         id      = aws_launch_template.web_server_as.id
         version = "$Latest"
