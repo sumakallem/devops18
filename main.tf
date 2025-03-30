df537a8c200b2fdcb430e2d81117835f408fdca2
sumakallem <128606106+sumakallem@users.noreply.github.com>
2025-03-30 18:30:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_launch_template" "web_server_as" {
-    name = "project_mono_1"
+    name = "project_mono_2"
     image_id           = "ami-01bd9d8f06d29d6a0"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.nano"
@@ -11,7 +11,7 @@ resource "aws_launch_template" "web_server_as" {
 }
    
   resource "aws_elb" "web_server_lb"{
-     name = "web-server-lb"
+     name = "web-server-lb2"
      security_groups = [aws_security_group.web_server.id]
      subnets = ["subnet-01f2c32ef6cb62ecd", "subnet-04ce12a377063caa3"]
      listener {
@@ -25,13 +25,13 @@ resource "aws_launch_template" "web_server_as" {
     }
   }
 resource "aws_autoscaling_group" "web_server_asg" {
-    name                 = "web-server-asg"
+    name                 = "web-server-asg2"
     min_size             = 1
     max_size             = 3
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    #availability_zones    = ["ap-south-1b", "ap-south-1a"] 
+    availability_zones    = ["ap-south-1b", "ap-south-1a"] 
     launch_template {
         id      = aws_launch_template.web_server_as.id
         version = "$Latest"
